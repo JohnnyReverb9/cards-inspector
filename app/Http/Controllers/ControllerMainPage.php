5929b356b9063cdfdf55ce3ecc5c6a8dedaca686
Stepan <stepan.rulyov@gmail.com>
2024-07-23 11:56:29 +0300

    Создан контроллер для главной страницы и её отображение

---
+++ app/Http/Controllers/ControllerMainPage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ControllerMainPage
+{
+    public function index()
+    {
+        return view("main_page/main_page");
+    }
+}
---
+++ resources/views/main_page/main_page.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.app")
+
+@section("content")
+
+    <div>
+        <h1>Hi!</h1>
+    </div>
+
+@endsection
