f74a542c69ccf5ec9eb5a59ca9361671506ac9ef
Stepan <stepan.rulyov@gmail.com>
2024-08-02 01:10:48 +0300

    Лог с созданием картой

--- app/Http/Controllers/ControllerCardManagement.php
+++ app/Http/Controllers/ControllerCardManagement.php
@@ -24,7 +24,6 @@ class ControllerCardManagement extends Controller
                 "flat_num" => "required|numeric|min:1|max:561",
                 "phone" => "required|string|min:18|max:18",
                 "alias" => "required|string|min:4|max:100",
-                // "expiration" => "nullable|date_format:Y-m-d",
                 "passport" => "string|min:6|max:10"
             ]);
 
@@ -41,7 +40,7 @@ class ControllerCardManagement extends Controller
 
             $card = Card::create($res);
 
-            return redirect('/view_cards')->with('success', 'Карта успешно создана');
+            return redirect("/view_cards")->with("success", "Карта №" . $card->id . " успешно создана.");
         }
         catch (\Illuminate\Database\QueryException $e)
         {
@@ -57,7 +56,6 @@ class ControllerCardManagement extends Controller
     {
         $card_list = Card::all();
 
-        // dd($card_list);
         return view("view_cards/view_card_list", ["card_list" => $card_list]);
     }
 }
