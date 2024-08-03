f3756a0b5520e7c86e63468b9a058a2519462e35
Stepan <stepan.rulyov@gmail.com>
2024-08-03 13:14:21 +0300

    Уменьшено кол-во информации для отображения в карточках

--- resources/views/create_card/create_card.blade.php
+++ resources/views/create_card/create_card.blade.php
@@ -2,6 +2,9 @@
 
 @section("content")
     <div class="container">
+        <div class="row justify-content-end">
+            <a href="{{ url("/home") }}" class="btn btn-primary" style="width: 150px">На главную</a>
+        </div>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
--- resources/views/view_cards/view_card_list.blade.php
+++ resources/views/view_cards/view_card_list.blade.php
@@ -3,6 +3,9 @@
 @section("content")
 
     <div class="container">
+        <div class="row justify-content-end">
+            <a href="{{ url("/home") }}" class="btn btn-primary" style="width: 150px">На главную</a>
+        </div>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -11,12 +14,9 @@
                         @forelse($card_list as $card)
                             <div class="card card-in-list" style="margin: 10px; position: relative; cursor: pointer;" onclick="window.location='{{ url('/view_card', $card->id) }}';">
                                 <div class="card-body">
-                                    <p><strong>Имя:</strong> {{ $card->full_name }}</p>
+                                    <p><strong>ФИО:</strong> {{ $card->full_name }}</p>
                                     <p><strong>Номер квартиры:</strong> {{ $card->flat_num }}</p>
-                                    <p><strong>Телефон:</strong> {{ $card->phone }}</p>
-                                    <p><strong>Псеводним:</strong> {{ $card->alias }}</p>
                                     <p><strong>Дата окончания:</strong>@if($card->expiration == null) Бессрочно @endif {{ $card->expiration }}</p>
-                                    <p><strong>Паспорт / свидетельство о рождении:</strong> {{ $card->passport }}</p>
                                     <p><strong>Добавил сотрудник:</strong> {{ $users[$card->staff_add]->name . " (ID: " . $card->staff_add . "; email: " . $users[$card->staff_add]->email . ")" }}</p>
                                 </div>
                             </div>
